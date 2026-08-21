3df9fa1164c492eb61877f4ad9d4342f738737b0
Seoyoung Hwang <nct830@naver.com>
2026-08-22 01:12:35 +0900

    feat : 회원가입 페이지 UI 구현

--- src/pages/signup/SignupPage.tsx
+++ src/pages/signup/SignupPage.tsx
@@ -6,13 +6,14 @@ import { useState } from "react";
 import EyeIcon from "@/components/icons/EyeIcon";
 import EyeOffIcon from "@/components/icons/EyeOffIcon";
 
-const LoginPage = () => {
+const SignupPage = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
 
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="border-border flex flex-col items-center gap-10 rounded-md border px-10 py-15">
-        <div className="text-2xl font-bold">로그인</div>
+        <div className="text-2xl font-bold">회원가입</div>
 
         <div>
           <div className="mb-1.5 text-base font-bold">아이디</div>
@@ -42,18 +43,39 @@ const LoginPage = () => {
               )}
             </button>
           </div>
+
+          <div className="mt-5 mb-1.5 text-base font-bold">비밀번호 확인</div>
+          <div className="relative w-85">
+            <input
+              type={showPasswordConfirm ? "text" : "password"}
+              className="hover:border-input-border-hover active:border-input-border-hover border-border bg-input w-full rounded-md border py-3 pr-11 pl-3 text-sm"
+              placeholder="비밀번호를 다시 입력해주세요."
+            />
+
+            <button
+              type="button"
+              onClick={() => setShowPasswordConfirm((prev) => !prev)}
+              className="absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer"
+            >
+              {showPasswordConfirm ? (
+                <EyeOffIcon className="text-placeholder size-3" />
+              ) : (
+                <EyeIcon className="text-placeholder size-3" />
+              )}
+            </button>
+          </div>
         </div>
 
         <button
           type="submit"
           className="bg-primary hover:bg-primary-hover active:bg-primary-active text-inverse w-full cursor-pointer rounded-md py-3"
         >
-          로그인
+          회원가입
         </button>
         <div>
-          계정이 없으신가요?&nbsp;
-          <Link href="/signup" className="text-brand hover:text-brand-hover font-semibold">
-            회원가입
+          이미 계정이 있으신가요?&nbsp;
+          <Link href="/login" className="text-brand hover:text-brand-hover font-semibold">
+            로그인
           </Link>
         </div>
       </div>
@@ -61,4 +83,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default SignupPage;
