7a440fea9e12c0b046691eb208231cf93aa77908
Seoyoung Hwang <nct830@naver.com>
2026-08-25 01:58:08 +0900

    feat : 상세페이지 공연 좌석 별 금액 표시 및 좌석 별 컬러 코드 global.css 추가

--- src/pages/detail/components/ticketDetailSection/TicketDetailSection.tsx
+++ src/pages/detail/components/ticketDetailSection/TicketDetailSection.tsx
@@ -1,5 +1,6 @@
 import Button from "@/components/common/Button";
 
+import SeatPriceSection from "./SeatPriceSection";
 import TicketInformation from "./TicketInformation";
 import TicketIntroduction from "./TicketIntroduction";
 import TicketNotice from "./TicketNotice";
@@ -12,6 +13,7 @@ const TicketDetailSection = () => {
       <div className="mb-2 md:col-span-2 md:mb-3">
         <TicketNotice />
       </div>
+      <SeatPriceSection />
       <Button variant="primary" className="w-full py-4 md:col-span-2 md:py-4">
         예매하기
       </Button>
